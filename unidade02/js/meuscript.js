dc1f4c5b8513c4d887632dcd3698592b00527bad
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-01-14 19:09:56 -0300

    Estatísticas do vídeo - Aula 269

--- unidade02/js/meuscript.js
+++ unidade02/js/meuscript.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     $.get("https://www.googleapis.com/youtube/v3/channels", {
             part: 'contentDetails',
             forUsername: nomeCanal,
-            key: 'AIzaSyB49WfTkgfK2menTbmVCkLG0f9cYWQ9XKU'},
+            key: 'AIzaSyAcPYkguKj7oqrRrADgFl5VU2hDjAe9frA'},
             function(data) {
                 upload_id = data.items[0].contentDetails.relatedPlaylists.uploads;
                 pegarVideos(upload_id);
@@ -17,7 +17,7 @@ $(document).ready(function() {
                     part:'snippet',
                     maxResults: 12,
                     playlistId: id,
-                    key: 'AIzaSyB49WfTkgfK2menTbmVCkLG0f9cYWQ9XKU'},
+                    key: 'AIzaSyAcPYkguKj7oqrRrADgFl5VU2hDjAe9frA'},
             function(data) {
                 var imagem;
                 var arquivo;
--- unidade03/js/meuscript.js
+++ unidade03/js/meuscript.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    $.get("", {
-            part: '',
-            id: '',
-            key: 'AIzaSyB49WfTkgfK2menTbmVCkLG0f9cYWQ9XKU'},
+    $.get("https://www.googleapis.com/youtube/v3/videos", {
+            part: 'statistics',
+            id: 'AK5RptOd3k4',
+            key: 'AIzaSyAcPYkguKj7oqrRrADgFl5VU2hDjAe9frA'},
             function(data) {
                 console.log(data);
             }
