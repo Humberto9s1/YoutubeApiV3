7868b2590d08de6d8b749c768da8b91ab2e38b9f
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-01-14 15:44:35 -0300

    Conectar ao YouTube via API versão 3 - Aula 262

---
+++ js/meuscript.js
@@ -0,0 +1,20 @@
+var nomeCanal = 'backtotriangle';
+var upload_id;
+
+$(document).ready(function() {
+    $.get("https://www.googleapis.com/youtube/v3/channels", {
+        part: 'contentDetails',
+        forUsername: nomeCanal,
+        key: 'AIzaSyAcPYkguKj7oqrRrADgFl5VU2hDjAe9frA'
+    },
+        function(data) {
+            upload_id = data.items[0].contentDetails.relatedPlaylists.uploads;
+            pegarVideos(upload_id);
+        }
+    )
+
+    function pegarVideos(id){
+        console.log(id);
+    }
+
+});
